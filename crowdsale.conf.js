8a36814c0020bd513bcea66e7e5a3890ed652ba9
Miran L <miran@cofound.it>
2018-07-10 20:25:19 +0200

    Fix space width

--- crowdsale.conf.js
+++ crowdsale.conf.js
@@ -5,24 +5,24 @@ const DECIMALS = 18
 const EXP = web3.toBigNumber(10).pow(DECIMALS) // 1e18
 
 module.exports = {
-    token: {
-        standard: ' v1.0',
-        name: 'My Token',
-        symbol: 'MTK',
-        decimals: DECIMALS,
-    },
-    crowdsale: {
-        presaleStartTime: 1519142400,
-        presaleUnlimitedStartTime: 1519315200, //22/2/2017/1700
-        crowdsaleStartTime: 1519747200, //27/2/2017/1700
-        crowdsaleEndedTime: 1521561600, //20/3/2017/1700
-        minCap: web3.toWei(1, 'ether'),
-        maxCap: web3.toWei(100, 'ether'),
-        maxP1Cap: web3.toWei(100, 'ether'),
-        ethToTokenConversion: 47000,
-        maxTokenSupply: web3.toBigNumber(1000000000).times(EXP),
-        presaleBonusTokens: web3.toBigNumber(115996000).times(EXP),
-        presaleBonusAddress: '0xd7c4af0e30ec62a01036e45b6ed37bc6d0a3bd53',
-        presaleBonusAddressColdStorage: '0x47d634ce50170a156ec4300d35be3b48e17caaf6',
-    },
+  token: {
+    standard: ' v1.0',
+    name: 'My Token',
+    symbol: 'MTK',
+    decimals: DECIMALS,
+  },
+  crowdsale: {
+    presaleStartTime: 1519142400,
+    presaleUnlimitedStartTime: 1519315200, //22/2/2017/1700
+    crowdsaleStartTime: 1519747200, //27/2/2017/1700
+    crowdsaleEndedTime: 1521561600, //20/3/2017/1700
+    minCap: web3.toWei(1, 'ether'),
+    maxCap: web3.toWei(100, 'ether'),
+    maxP1Cap: web3.toWei(100, 'ether'),
+    ethToTokenConversion: 47000,
+    maxTokenSupply: web3.toBigNumber(1000000000).times(EXP),
+    presaleBonusTokens: web3.toBigNumber(115996000).times(EXP),
+    presaleBonusAddress: '0xd7c4af0e30ec62a01036e45b6ed37bc6d0a3bd53',
+    presaleBonusAddressColdStorage: '0x47d634ce50170a156ec4300d35be3b48e17caaf6',
+  },
 }
